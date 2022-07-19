041368fdc30f25b4ac27a45dd8c3f759bac4491c
jnchen12a <jnchen12a@gmail.com>
2022-07-19 11:26:54 -0500

    Changed lists to include more characters and added initial wheel shifts. However, machine does not work properly.

--- base_machine.py
+++ base_machine.py
@@ -2,22 +2,23 @@ from os import system, name #For clearing function
 
 from time import sleep #For adding delays
 
+from random import randint
+
 #Wheels
-owheel1_1 = ['Q', 'N', 'S', 'Y', 'D', 'I', 'V', 'B', 'U', 'F', 'K', 'H', 'C', 'O', 'J', 'T', 'A', 'G', 'R', 'X', 'M', 'P', 'E', 'Z', 'L', 'W']
-owheel1_2 = ('Z', 'H', 'C', 'M', 'Q', 'B', 'X', 'E', 'D', 'U', 'F', 'V', 'W', 'S', 'A', 'T', 'G', 'I', 'J', 'P', 'L', 'Y', 'O', 'R', 'N', 'K')
-wheel1_2 = ['Z', 'H', 'C', 'M', 'Q', 'B', 'X', 'E', 'D', 'U', 'F', 'V', 'W', 'S', 'A', 'T', 'G', 'I', 'J', 'P', 'L', 'Y', 'O', 'R', 'N', 'K']
+owheel1_1 = ('v', 'j', '1', '6', '5', '@', '8', 'M', 'Y', 'y', '_', 'n', 'L', '|', '4', 'O', '{', 'F', 'V', '<', ',', 'T', 't', ':', '=', 'J', '2', 'm', 'R', 'z', 'K', '/', 'H', 'W', ']', 'g', 'B', 'x', 'Z', 'i', '0', '?', 'U', '%', ';', '!', 'Q', 'e', '#', 'w', 'o', '>', 'I', '7', 'q', '(', '}', ' ', '\\', '.', '`', '3', 'S', '*', 'c', 'b', 'l', 'N', "'", 'A', 'C', '9', 's', 'r', 'h', 'E', '+', 'd', '$', 'P', '-', 'u', 'X', 'f', '~', '"', 'a', '^', 'k', 'D', ')', '[', 'p', 'G', '&')
+owheel1_2 = ("w", "_", "e", "2", ">", "Q", '"', ";", "9", "u", "<", "p", "1", "m", "P", "/", "q", "}", "J", "F", "v", "$", "[", "(", "~", "#", "x", "%", "U", "Z", "^", "j", "3", "E", "|", "s", "N", "d", "&", "b", "`", "y", "6", ")", "G", "t", "k", "f", "l", "i", "S", "W", "{", "8", "!", "M", "D", "=", "7", "?", "h", "a", "@", "R", "\\", "z", "0", "-", "O", " ", "r", "A", "5", "I", "]", ".", "L", "T", "X", "g", "Y", "*", ",", "K", "H", "4", "C", "+", "n", ":", "V", "o", "B", "c", "'")
+wheel1_2 = ["w", "_", "e", "2", ">", "Q", '"', ";", "9", "u", "<", "p", "1", "m", "P", "/", "q", "}", "J", "F", "v", "$", "[", "(", "~", "#", "x", "%", "U", "Z", "^", "j", "3", "E", "|", "s", "N", "d", "&", "b", "`", "y", "6", ")", "G", "t", "k", "f", "l", "i", "S", "W", "{", "8", "!", "M", "D", "=", "7", "?", "h", "a", "@", "R", "\\", "z", "0", "-", "O", " ", "r", "A", "5", "I", "]", ".", "L", "T", "X", "g", "Y", "*", ",", "K", "H", "4", "C", "+", "n", ":", "V", "o", "B", "c", "'"]
 
-owheel2_1 = ['P', 'X', 'T', 'Z', 'K', 'Y', 'F', 'O', 'Q', 'D', 'N', 'W', 'G', 'H', 'U', 'I', 'M', 'C', 'J', 'E', 'S', 'B', 'L', 'R', 'V', 'A']
-owheel2_2 = ('A', 'U', 'V', 'Q', 'L', 'H', 'I', 'Z', 'Y', 'P', 'T', 'K', 'S', 'E', 'G', 'D', 'M', 'B', 'O', 'N', 'J', 'X', 'F', 'R', 'W', 'C')
-wheel2_2 = ['A', 'U', 'V', 'Q', 'L', 'H', 'I', 'Z', 'Y', 'P', 'T', 'K', 'S', 'E', 'G', 'D', 'M', 'B', 'O', 'N', 'J', 'X', 'F', 'R', 'W', 'C']
+owheel2_1 = ('x', 'X', '$', 's', 'L', 'e', 'V', '*', 'G', '`', '{', 'c', 'Y', '(', 'm', 'r', '7', '2', 'Z', '?', '^', 'h', '1', 'P', 'q', 'j', 'O', '+', '@', '"', '#', '%', 'A', 'u', 'H', 'F', '}', 'W', 't', 'U', 'b', '3', 'g', '/', 'B', 'R', 'M', 'v', 'i', '!', ':', ')', '|', 'S', 'J', '=', 'C', '.', '&', ' ', 'z', '\\', '[', '5', 'k', ',', 'l', "'", 'w', 'y', 'T', '-', 'E', '>', 'I', ';', 'd', 'Q', 'n', '~', '_', 'p', '8', '4', 'N', '<', 'o', ']', '6', 'f', 'K', '0', 'D', 'a', '9')
+owheel2_2 = ('(', '1', ';', 'V', '}', 'l', 'j', 'E', 'B', 'p', '=', 'k', 'O', '5', '|', ':', 'D', '7', ')', 'Y', '~', 'X', '6', 'T', 'Z', 'r', '%', 'h', '?', 'o', '8', 'f', '*', 'H', 'w', '4', 'm', 'u', 'W', 't', '^', '.', 'S', '"', '<', 'd', '[', '$', 'g', '+', 'U', 'v', ']', '>', 'K', 'J', 'x', 'M', 'F', '/', 'L', 'e', 'i', 'C', 'A', 'I', '-', ' ', '3', "'", '9', 'q', '\\', '`', 'P', 'b', 'c', 'G', 'n', '2', '_', '&', 's', 'y', '{', '@', '!', '0', 'z', '#', ',', 'R', 'a', 'Q', 'N')
+wheel2_2 = ['(', '1', ';', 'V', '}', 'l', 'j', 'E', 'B', 'p', '=', 'k', 'O', '5', '|', ':', 'D', '7', ')', 'Y', '~', 'X', '6', 'T', 'Z', 'r', '%', 'h', '?', 'o', '8', 'f', '*', 'H', 'w', '4', 'm', 'u', 'W', 't', '^', '.', 'S', '"', '<', 'd', '[', '$', 'g', '+', 'U', 'v', ']', '>', 'K', 'J', 'x', 'M', 'F', '/', 'L', 'e', 'i', 'C', 'A', 'I', '-', ' ', '3', "'", '9', 'q', '\\', '`', 'P', 'b', 'c', 'G', 'n', '2', '_', '&', 's', 'y', '{', '@', '!', '0', 'z', '#', ',', 'R', 'a', 'Q', 'N']
 
-owheel3_1 = ['W', 'D', 'I', 'B', 'K', 'L', 'X', 'P', 'J', 'R', 'Q', 'Z', 'V', 'T', 'N', 'G', 'Y', 'U', 'S', 'H', 'C', 'A', 'M', 'E', 'F', 'O']
-owheel3_2 = ('Y', 'S', 'W', 'L', 'P', 'E', 'G', 'Z', 'I', 'H', 'U', 'N', 'D', 'J', 'X', 'B', 'F', 'K', 'Q', 'C', 'V', 'R', 'T', 'A', 'O', 'M')
-wheel3_2 = ['Y', 'S', 'W', 'L', 'P', 'E', 'G', 'Z', 'I', 'H', 'U', 'N', 'D', 'J', 'X', 'B', 'F', 'K', 'Q', 'C', 'V', 'R', 'T', 'A', 'O', 'M']
+owheel3_1 = ('&', '*', '_', 'z', 'c', '}', 'V', 'n', 'r', 'W', '2', '+', 'U', 'Q', 'v', '@', 'w', '(', 'T', '0', 'X', 'O', 'K', '>', '!', 'i', '^', '`', ';', 'A', 'P', '-', '1', '8', "'", '=', 'o', '3', ',', 'k', 't', ')', 'p', '$', 'D', 'F', '%', ']', '4', 'm', 'B', 'j', '~', '"', 'a', 'N', '?', 'S', 'e', '|', ':', '5', 's', 'g', '{', 'y', 'q', 'I', 'u', '#', 'C', 'f', '<', ' ', 'L', 'b', 'Z', '/', '9', 'H', '.', 'h', 'd', '6', '7', 'l', 'R', 'G', '\\', 'E', 'J', 'Y', 'M', '[', 'x')
+owheel3_2 = ('>', 'K', 'J', '7', "'", 'j', '!', ';', '_', '{', 'A', '6', 'h', 'P', '%', 'Q', 'k', 'o', ' ', '(', '0', '^', 'I', 'z', '&', '5', 'N', '.', 'Z', '~', 'E', 'b', 'C', '`', '@', 'c', '+', '8', 'Y', '[', ',', 'G', 'n', 'a', 'U', '/', 'D', 'w', 'm', '}', 'd', 'H', '3', ')', ']', '*', 'v', 'g', 'e', 'T', 'V', ':', 'O', 'r', 'f', 'M', 'B', '1', '#', '9', 'X', 'x', '?', 'y', 'S', 'u', '|', 't', 'W', '"', '$', 'L', 'R', 'F', 'q', '=', 'p', '-', '4', '<', 'l', '\\', '2', 's', 'i')
+wheel3_2 = ['>', 'K', 'J', '7', "'", 'j', '!', ';', '_', '{', 'A', '6', 'h', 'P', '%', 'Q', 'k', 'o', ' ', '(', '0', '^', 'I', 'z', '&', '5', 'N', '.', 'Z', '~', 'E', 'b', 'C', '`', '@', 'c', '+', '8', 'Y', '[', ',', 'G', 'n', 'a', 'U', '/', 'D', 'w', 'm', '}', 'd', 'H', '3', ')', ']', '*', 'v', 'g', 'e', 'T', 'V', ':', 'O', 'r', 'f', 'M', 'B', '1', '#', '9', 'X', 'x', '?', 'y', 'S', 'u', '|', 't', 'W', '"', '$', 'L', 'R', 'F', 'q', '=', 'p', '-', '4', '<', 'l', '\\', '2', 's', 'i']
 
 wheel1_2moves = 0
 wheel2_2moves = 0
-wheel3_2moves = 0
 
 #Function that clears terminal
 def clear():
@@ -38,10 +39,25 @@ def encoding_function(message):
     global wheel3_2
     global wheel1_2moves
     global wheel2_2moves
-    global wheel3_2moves
 
-    message = message.upper()
+    wheel1_2moves = 0
+    wheel2_2moves = 0
+
     message = list(message)
+
+    #Generating random wheel start positions
+    value1 = randint(0, 63) % 25
+    value2 = randint(0, 63) % 25
+    value3 = randint(0, 63) % 25
+
+    returnmes += str(owheel1_1[value3])
+    returnmes += str(owheel1_1[value2])
+    returnmes += str(owheel1_1[value1])
+
+    startshift = value1 + (value2 * 25) + (value3 * 625)
+    shiftwheel(startshift)
+
+    #Generating encoded message
     for character in message:
         if character in owheel1_1:
             index = owheel1_1.index(character)
@@ -62,9 +78,6 @@ def encoding_function(message):
     wheel1_2 = list(owheel1_2)
     wheel2_2 = list(owheel2_2)
     wheel3_2 = list(owheel3_2)
-    wheel1_2moves = 0
-    wheel2_2moves = 0
-    wheel3_2moves = 0
     return returnmes
 
 #Decoding function
@@ -82,10 +95,25 @@ def decoding_function(message):
     global wheel3_2
     global wheel1_2moves
     global wheel2_2moves
-    global wheel3_2moves
 
-    message = message.upper()
+    wheel1_2moves = 0
+    wheel2_2moves = 0
+
     message = list(message)
+
+    #Decoding starting wheel positions
+    chr1 = message.pop(0)
+    chr2 = message.pop(0)
+    chr3 = message.pop(0)
+
+    value1 = owheel1_1.index(chr1) % 25
+    value2 = owheel1_1.index(chr2) % 25
+    value3 = owheel1_1.index(chr3) % 25
+
+    startshift = value1 + (value2 * 25) + (value3 * 625)
+    shiftwheel(startshift)
+
+    #Decoding actual message
     for character in message:
         if character in wheel3_2:
             index = wheel3_2.index(character)
@@ -104,9 +132,6 @@ def decoding_function(message):
     wheel1_2 = list(owheel1_2)
     wheel2_2 = list(owheel2_2)
     wheel3_2 = list(owheel3_2)
-    wheel1_2moves = 0
-    wheel2_2moves = 0
-    wheel3_2moves = 0
     
     return returnmes
 
@@ -114,13 +139,12 @@ def decoding_function(message):
 def shiftwheel(times):
     global wheel1_2moves
     global wheel2_2moves
-    global wheel3_2moves
 
     global wheel1_2
     global wheel2_2
     global wheel3_2
     
-    for n in range(times):
+    for n in range(times - 1):
         endchar = wheel1_2.pop()
         wheel1_2.insert(0, endchar)
         wheel1_2moves += 1
@@ -168,6 +192,8 @@ while True: #While loop 1
             print(encoding_function(encodingmes))
             print("Success! The above characters are your encoded message.")
             print("Please make sure to copy them.")
+            print(wheel1_2)
+            print(wheel1_2moves)
             sleep(2)
             break
             #More code
